4a83ecba2fd466feddef7057f7ae45c7823934ee
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2023-09-07 01:06:02 +0530

    finished

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var randomN = Math.floor(Math.random()*10)+1
+console.log("Random number is :"+randomN)
+
+var score = 10
+
+function check() {
+    var input = document.getElementById("inputBox")
+    var n = Number(input.value)
+    console.log("Input is :"+n)
+    var r = document.getElementById("result")
+    var o = document.getElementById("out")
+
+    if(n>=1 && n<=10){
+        if(randomN == n){
+            console.log("You are Right!") 
+            r.textContent = "Your guess is right!"
+        }
+        else{
+            score--
+            console.log("You are wrong!")
+            r.textContent = "Your guess is wrong!"
+            o.textContent = "Your Score: "+ score
+            console.log("Your Score: "+ score)
+        }
+    }
+    else {
+        r.textContent = "Enter number between 1 to 10!"
+    }
+}
